1e2476270300f579fde68cfa816a87a1b6fd514e
TheVitik <70289785+TheVitik@users.noreply.github.com>
2021-06-04 01:35:27 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -6,16 +6,14 @@ public class Main {
     public static void main(String[] args){
         Article[] articles = new Article[4];
         articles[0] = new PaperArticle("Viktor",3200);
-        //articles[1] = new ElectronicArticle("Nikita","29.11.2020",10,"Calibri",7382,18,"/videos/article1/news.mp4");
-        articles[1] = new PaperArticle("Evgeniy",1444);
-        articles[2] = new PaperArticle("Admin",1242);
+        articles[1] = new ElectronicArticle("Nikita",7382,10);
+        articles[2] = new PaperArticle("Evgeniy",1444);
+        articles[3] = new PaperArticle("Admin",1242);
         Article.ArticleComparator acomp = new Article.ArticleComparator();
-
         TreeSet<Article> treeset = new TreeSet<Article>(acomp);
         treeset.addAll(Arrays.asList(articles));
         for (Article i:treeset){
             System.out.println(i.getAuthor() + " " + i.getSymbols());
         }
-
     }
 }
